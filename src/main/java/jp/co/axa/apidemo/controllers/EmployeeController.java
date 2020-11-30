08011f4c26483fb6cea6c7d20e5c1b470742a0c3
Jithin <jithinbabu657@gmail.com>
2020-11-30 21:11:50 +0900

    Implementing cache

--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -4,12 +4,14 @@ import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.services.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@EnableCaching
 @RequestMapping("/api/v1")
 @Slf4j
 public class EmployeeController {
--- src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import springfox.documentation.annotations.Cacheable;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,11 +19,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         this.employeeRepository = employeeRepository;
     }
 
+    @Cacheable("retrieveEmployees")
     public List<Employee> retrieveEmployees() {
         List<Employee> employees = employeeRepository.findAll();
         return employees;
     }
 
+    @Cacheable("getEmployee")
     public Employee getEmployee(Long employeeId) {
         Optional<Employee> optEmp = employeeRepository.findById(employeeId);
         return optEmp.get();
